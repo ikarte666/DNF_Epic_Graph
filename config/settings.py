b9b20d31961083627bf409643ac6273ac939b878
Geonju Park <pkju98@naver.com>
2020-08-28 22:57:26 +0900

    배포중(static폴더 수정)

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "www.dfgraph.xyz"]
+ALLOWED_HOSTS = ["127.0.0.1", "www.dfgraph.xyz", "dfgraph.xyz"]
 
 
 # Application definition
